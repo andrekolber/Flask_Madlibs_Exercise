2fa1846da53aaf2e21df3d1efcda761d56b45913
Andre Kolber <andrekolber1@gmail.com>
2021-10-22 12:51:08 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'asdasdasd'
+
+debug = DebugToolbarExtension(app)
+
+"""Madlibs Stories."""
+
+
+class Story:
+    """Madlibs story.
+
+    To  make a story, pass a list of prompts, and the text
+    of the template.
+
+        >>> s = Story(["noun", "verb"],
+        ...     "I love to {verb} a good {noun}.")
+
+    To generate text from a story, pass in a dictionary-like thing
+    of {prompt: answer, promp:answer):
+
+        >>> ans = {"verb": "eat", "noun": "mango"}
+        >>> s.generate(ans)
+        'I love to eat a good mango.'
+    """
+
+    def __init__(self, words, text):
+        """Create story with words and template text."""
+
+        self.prompts = words
+        self.template = text
+
+    def generate(self, answers):
+        """Substitute answers into text."""
+
+        text = self.template
+
+        for (key, val) in answers.items():
+            text = text.replace("{" + key + "}", val)
+
+        return text
+
+
+# Here's a story to get you started
+
+
+story = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
+@app.route('/form')
+def home_page():
+    return render_template('app.html')
+
+@app.route('/story')
+def story():
+    place = request.args.get('place')
+    noun = request.args.get('noun')
+    verb = request.args.get('verb')
+    adjective = request.args.get('adjective')
+    plural_noun = request.args.get('plural_noun')
+    return render_template('story.html', place = place, noun = noun, 
+    verb = verb, adjective = adjective, plural_noun = plural_noun)
